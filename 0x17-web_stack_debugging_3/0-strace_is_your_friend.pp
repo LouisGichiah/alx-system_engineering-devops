9601d2fac9dce0a60f6dd8bedc562ddf687dd0d8
LouisGichiah <louiegpaul@gmail.com>
2022-11-02 10:43:55 -0700

    fix it

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # debug and then automate it using Puppet
-exec { 'fix_phpp':
+exec { 'fix_wp':
 	provider => 'shell',
-	command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+	command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path    => '/usr/local/bin/:/bin/'
 }
