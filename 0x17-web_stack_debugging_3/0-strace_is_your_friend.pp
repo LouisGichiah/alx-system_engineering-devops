96885cf8adf92af5543b28b4af42ce050559fff7
LouisGichiah <louiegpaul@gmail.com>
2022-11-02 17:45:07 +0000

    fix it

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,5 @@
 exec { 'fix_wp':
 	provider => 'shell',
 	command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-    path    => '/usr/local/bin/:/bin/'
+        path    => '/usr/local/bin/:/bin/'
 }
